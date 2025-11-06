b69806bc4d841447c20d0d50f0da4fceb63c9c72
ghir-hak <amine.zaoui@taubyte.com>
2025-11-06 16:24:34 +0100

    a

--- functions/hello_world/hello_world.go
+++ functions/hello_world/hello_world.go
@@ -4,14 +4,14 @@ import (
 	"github.com/taubyte/go-sdk/event"
 )
 
-//export ping
+//export helloworld
 func ping(e event.Event) uint32 {
 	h, err := e.HTTP()
 	if err != nil {
 		return 1
 	}
 
-	h.Write([]byte("PONG"))
+	h.Write([]byte("Hello world"))
 
 	return 0
 }
